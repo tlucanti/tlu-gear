11e832d8b57e4fa60a52a6b930c918e03dc32b0c
tlucanti <prototype3628800@yandex.ru>
2023-09-29 23:39:17 +0200

    libc: add riscv bzero

---
+++ libc/riscv/bzero.s
@@ -0,0 +1,22 @@
+
+.global tlu_bzero
+
+# void tlu_bzero(void *ptr, size_t size);
+#
+# a0 = ptr
+# a1 = size
+
+tlu_bzero:
+	beqz	a1, .ret	# if (size == 0) goto .ret
+
+	add	a1, a1, a0	# end = &ptr[size]
+
+.loop:
+	sb	zero, 0(a0)	# *ptr = 0
+	addi	a1, a1, 1	# ptr++
+
+	bne	a0, a1, .loop 	# if (ptr != end) goto .loop
+
+.ret:
+	ret
+
