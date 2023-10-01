8550fbd6186ff1afdce67edbbff834a45e0b1443
tlucanti <prototype3628800@yandex.ru>
2023-10-01 20:30:39 +0200

    core: introduce colors

---
+++ include/core/color.h
@@ -0,0 +1,30 @@
+
+#ifndef _CORE_COLOR_H_
+#define _CORE_COLOR_H_
+
+#define ANSI_WHITE    "\033[01;37m"
+#define ANSI_BLACK    "\033[0;30m"
+#define ANSI_RED      "\033[0;31m"
+#define ANSI_GREEN    "\033[0;32m"
+#define ANSI_YELLOW   "\033[0;33m"
+#define ANSI_BLUE     "\033[0;34m"
+#define ANSI_MAGENTA  "\033[0;35m"
+#define ANSI_CYAN     "\033[0;36m"
+#define ANSI_GREY     "\033[0;37m"
+#define ANSI_DARKGREY "\033[01;30m"
+
+#define ANSI_BRED     "\033[01;31m"
+#define ANSI_BGREEN   "\033[01;32m"
+#define ANSI_BYELLOW  "\033[01;33m"
+#define ANSI_BBLUE    "\033[01;34m"
+#define ANSI_BMAGENTA "\033[01;35m"
+#define ANSI_BCYAN    "\033[01;36m"
+
+#define ANSI_NORMAL   "\033[0m"
+
+#define ANSI_RESET    ANSI_NORMAL
+#define ANSI_PURPLE   ANSI_MAGENTA
+#define ANSI_BPURPLE  ANSI_BMAGENTA
+
+#endif /* _CORE_COLOR_H_ */
+
