d23081721bffc4d2a478ff36045f3b03809d9453
tlucanti <prototype3628800@yandex.ru>
2023-10-15 00:51:11 +0200

    libc: add memcmp to libc, fixed memcmp

--- include/libc/libc.h
+++ include/libc/libc.h
@@ -3,4 +3,5 @@
 
 void tlu_memset(void *ptr, unsigned char chr, size_t size);
 void tlu_bzero(void *ptr, size_t size);
+int tlu_memcmp(const void *va, const void *vb, size_t size);
 
--- libc/generic-native/memcmp.c
+++ libc/generic-native/memcmp.c
@@ -1,14 +1,14 @@
 
 #include <stddef.h>
 
-int memcmp(const void *va, const void *vb, size_t size)
+int tlu_memcmp(const void *va, const void *vb, size_t size)
 {
 	const unsigned char *a = va;
 	const unsigned char *b = vb;
 
 	for (size_t i = 0; i < size; ++i) {
 		if (a[i] != b[i]) {
-			return b[i] - a[i];
+			return a[i] - b[i];
 		}
 	}
 	return 0;
