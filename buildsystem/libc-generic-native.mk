c99c4f4e4f8321103c2b11bc199c164bd2b7be97
tlucanti <prototype3628800@yandex.ru>
2023-12-10 14:09:13 +0000

    libc: restructured

--- buildsystem/libc-generic-native.mk
+++ buildsystem/libc-generic-native.mk
@@ -1,13 +1,18 @@
 
 TARGET = libcgn.a
-SRC = \
+MEM_SRC = \
       memzero.c memset.c memcmp.c memeq.c memnchr.c memchr.c memcpy.c \
-      memmove.c \
-      \
-      strlen.c strcmp.c streq.c strchr.c strrchr.c sstartswith.c \
-      \
+      memmove.c
+STR_SRC = \
+      strlen.c strcmp.c streq.c strchr.c strrchr.c sstartswith.c
+CHAR_SRC = \
       isalnum.c isalpha.c isdigit.c ishex.c islower.c isupper.c \
       isprint.c ispunct.c isspace.c tolower.c toupper.c
+
+SRC = $(addprefix mem/,$(MEM_SRC)) \
+      $(addprefix string/,$(STR_SRC)) \
+      $(addprefix char/,$(CHAR_SRC))
+
 FLAGS = -I include/
 DEPS =
 DIR = libc/generic-native
