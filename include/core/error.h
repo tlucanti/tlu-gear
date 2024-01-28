1bccbd70c961f07d66f684d90f8747f46a641dac
tlucanti <prototype3628800@yandex.ru>
2024-01-28 19:22:31 +0000

    core: add error codes file

--- include/core/error.h
+++ include/core/error.h
@@ -2,7 +2,18 @@
 #ifndef _CORE_ERROR_H_
 #define _CORE_ERROR_H_
 
-#define TLU_EFAULT 0x1
+#define	TLU_ENOENT		 2	/* not exists */
+#define	TLU_EIO			 5	/* I/O error */
+#define	TLU_ENOMEM		12	/* out of memory */
+#define	TLU_EFAULT		14	/* unrecoverable error */
+#define	TLU_EEXIST		17	/* exists */
+#define	TLU_EINVAL		22	/* invalid value */
+#define	TLU_EDEADLOCK		35	/* deadlock */
+#define	TLU_ENOSYS		38	/* not implemmented */
+#define	TLU_ENODATA		61	/* no data avaliable */
+#define	TLU_EOVERFLOW		75	/* overflow */
+#define	TLU_EBADFD		77	/* bad file descriptor */
+#define	TLU_ETIMEDOUT		110	/* timed out */
 
 #endif /* _CORE_ERROR_H_ */
 
