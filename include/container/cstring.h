3dbf2809b7c2dfbe9c6cc6b9b77cada19f023922
tlucanti <prototype3628800@yandex.ru>
2024-02-03 20:42:25 +0000

    cstring: update interface

--- include/container/cstring.h
+++ include/container/cstring.h
@@ -20,6 +20,9 @@ struct cstring {
 # define __may_alloc /* empty */
 #endif
 
+#define __DEBATING __unused
+#define __TODO __unused
+
 /**
  * constructors and destructors
  */
@@ -31,6 +34,7 @@ void cstring_destroy(struct cstring *cstring);
  */
 const char *cstring_data(const struct cstring *cstring);
 unsigned long cstring_size(const struct cstring *cstring);
+unsigned long cstring_alloc(const struct cstring *cstring);
 char cstring_at(const struct cstring *cstring, long pos);
 
 /**
@@ -38,8 +42,17 @@ char cstring_at(const struct cstring *cstring, long pos);
  */
 void cstring_set(struct cstring *cstring, long pos, char c);
 __may_alloc int cstring_append(struct cstring *cstring, char c);
+__may_alloc int cstring_appfront(struct cstring *cstring, char c) __TODO;
 __may_alloc int cstring_extend(struct cstring *cstring, const char *other);
-__may_alloc int cstring_pop(struct cstring *cstring, unsigned long nr);
+__may_alloc int cstring_extfront(struct cstring *cstring, const char *other) __TODO;
+__may_alloc int cstring_remove_suffix(struct cstring *cstring, unsigned long size) __TODO;
+__may_alloc int cstring_remove_prefix(struct cstring *cstring, unsigned long size) __TODO;
+
+/**
+ * string creation
+ */
+__may_alloc int cstring_copy(const struct cstring *src, struct cstring *dst) __DEBATING;
+__may_alloc int cstring_subs(const struct cstring *src, const struct cstring *dst, unsigned long begin, unsigned long end) __DEBATING;
 
 /**
  * string lookups
@@ -54,24 +67,23 @@ unsigned long cstring_rfind(const struct cstring *cstring, const char *pattern);
 /**
  * string character informaion
  */
-bool cstring_isalpha(const struct cstring *cstring);
-bool cstring_isdigit(const struct cstring *cstring);
-bool cstring_isalnum(const struct cstring *cstring);
-bool cstring_islower(const struct cstring *cstring);
-bool cstring_isupper(const struct cstring *cstring);
-bool cstring_isspace(const struct cstring *cstring);
+bool cstring_isalpha(const struct cstring *cstring) __TODO;
+bool cstring_isdigit(const struct cstring *cstring) __TODO;
+bool cstring_isalnum(const struct cstring *cstring) __TODO;
+bool cstring_islower(const struct cstring *cstring) __TODO;
+bool cstring_isupper(const struct cstring *cstring) __TODO;
+bool cstring_isspace(const struct cstring *cstring) __TODO;
 
 /**
- * cstring formattings
+ * cstring formatting
  */
-void cstring_tolwer(struct cstring *cstring);
-void cstring_toupper(struct cstring *cstring);
-void cstring_remove_prefix(struct cstring *cstring, const char *pattern);
-void cstring_remove_suffix(struct cstring *cstring, const char *pattern);
-__may_alloc int cstring_replace(struct cstring *cstring, const char *from, const char *to);
-void cstring_lstrip(struct cstring *cstring);
-void cstring_rstrip(struct cstring *cstring);
-void cstring_reverse(struct cstring *cstring);
+void cstring_tolwer(struct cstring *cstring) __TODO;
+void cstring_toupper(struct cstring *cstring) __TODO;
+__may_alloc int cstring_replace(struct cstring *cstring, const char *from, const char *to) __TODO;
+__may_alloc void cstring_lstrip(struct cstring *cstring) __TODO;
+__may_alloc void cstring_rstrip(struct cstring *cstring) __TODO;
+__may_alloc void cstring_strip(struct cstring *cstring) __TODO;
+void cstring_reverse(struct cstring *cstring) __TODO;
 
 #endif /* _CONTAINER_CSTRING_H_ */
 
