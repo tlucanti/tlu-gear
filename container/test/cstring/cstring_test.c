bb9b00c1310b6f6dcaf1290a5b42734f3d3ecbaa
tlucanti <prototype3628800@yandex.ru>
2024-02-03 20:42:58 +0000

    cstring: add extend(), remove_suffix() tests

--- container/test/cstring/cstring_test.c
+++ container/test/cstring/cstring_test.c
@@ -1,13 +1,18 @@
 
+#include <core/math.h>
+#include <core/panic.h>
 #include <container/cstring.h>
 #include <utest/utest.h>
 
 #include <string.h>
 
-static void util_cstring_cmp(const struct cstring *cstring, const char *s)
-{
-	ASSERT_EQUAL_STR(s, cstring_data(cstring));
-}
+#define type_check(type, var) (void)((type)NULL == (var))
+#define util_cstring_cmp(__cstr, __s)                        \
+	do {                                                 \
+		type_check(const struct cstring *, __cstr);  \
+		type_check(const char *, __s);               \
+		ASSERT_EQUAL_STR(__s, cstring_data(__cstr)); \
+	} while (false)
 
 UTEST(cstring_init)
 {
@@ -116,7 +121,7 @@ UTEST(csrting_append)
 	cstring_append(&a, 'x');
 	util_cstring_cmp(&a, "123x");
 
-	for (int i = 0; i < 1000; ++i) {
+	for (unsigned long i = 0; i < ARRAY_SIZE(buf) - 1; ++i) {
 		buf[i] = 'x';
 		cstring_append(&b, 'x');
 		util_cstring_cmp(&b, buf);
@@ -126,6 +131,82 @@ UTEST(csrting_append)
 	cstring_destroy(&b);
 }
 
+UTEST(cstring_extend)
+{
+	struct cstring a, b;
+	char *ext = "12345";
+	char buf[1000 * 5 + 1] = {};
+
+	cstring_init(&a, "xyz");
+	cstring_init(&b, "");
+
+	cstring_extend(&a, ext);
+	util_cstring_cmp(&a, "xyz12345");
+
+	for (int i = 0; i < 1000; ++i) {
+		cstring_extend(&b, ext);
+		strcat(buf, ext);
+		BUG_ON(strlen(buf) >= ARRAY_SIZE(buf));
+		util_cstring_cmp(&b, buf);
+	}
+
+	cstring_destroy(&a);
+	cstring_destroy(&b);
+}
+
+UTEST(cstring_remove_prefix)
+{
+	struct cstring a, b;
+	char *ext = "12345";
+	char c[2] = {};
+	char bufa[1000 + 1] = {};
+	char bufb[1000 * 5 + 1] = {};
+	unsigned long bufa_size, bufb_size;
+
+	cstring_init(&a, "xyz");
+	cstring_init(&b, "");
+
+	cstring_remove_suffix(&a, 1);
+	util_cstring_cmp(&a, "xy");
+
+	cstring_remove_suffix(&a, 2);
+	util_cstring_cmp(&a, "");
+
+	cstring_remove_suffix(&b, 1000);
+	util_cstring_cmp(&b, "");
+
+
+	for (int i = 0; i < 1000; ++i) {
+		c[0] = 'a' + i % 26;
+
+		cstring_extend(&a, c);
+		strcat(bufa, c);
+		BUG_ON(strlen(bufa) >= ARRAY_SIZE(bufa));
+
+		cstring_extend(&b, ext);
+		strcat(bufb, ext);
+		BUG_ON(strlen(bufb) >= ARRAY_SIZE(bufb));
+	}
+	bufa_size = strlen(bufa);
+	bufb_size = strlen(bufb);
+
+	do {
+		cstring_remove_suffix(&a, 1);
+		bufa_size = max(bufa_size - 1, 0u);
+		bufa[bufa_size] = '\0';
+		util_cstring_cmp(&a, bufa);
+	} while ((long)bufa_size >= 0);
+
+	do {
+		cstring_remove_suffix(&b, 4);
+		bufb_size = max(bufb_size - 4, 0u);
+		bufb[bufb_size] = '\0';
+		util_cstring_cmp(&b, bufb);
+	} while ((long)bufb_size >= 0);
+
+	cstring_destroy(&a);
+	cstring_destroy(&b);
+}
 
 int main(int, const char **argv)
 {
