37f5a4cb9fd65f57bc28c89da05fc1401ced515b
tlucanti <prototype3628800@yandex.ru>
2024-02-08 21:56:33 +0000

    libc: simplified strrchr

--- libc/generic-native/string/strrchr.c
+++ libc/generic-native/string/strrchr.c
@@ -3,19 +3,12 @@
 
 const char *tlu_strrchr(const char *str, char c)
 {
-	const char *ret = NULL;
-
-	if (*str == c) {
-		ret = str;
-	}
-
-	while (*str) {
-		++str;
+	do {
 		if (*str == c) {
-			ret = str;
+			return str;
 		}
-	}
+	} while (*str++);
 
-	return ret;
+	return NULL;
 }
 
