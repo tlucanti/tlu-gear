dd68886caeefb605b0adc622b798c2369c545725
tlucanti <prototype3628800@yandex.ru>
2024-06-25 23:03:46 +0300

    introduce core types
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

---
+++ include/core/types.h
@@ -0,0 +1,23 @@
+
+#ifndef CORE_TYPES_H
+#define CORE_TYPES_H
+
+typedef _Bool bool;
+typedef unsigned int uint;
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long int64;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+#define true (bool)1
+#define false (bool)0
+#define NULL (void *)0UL
+
+#endif /* CORE_TYPES_H */
+
