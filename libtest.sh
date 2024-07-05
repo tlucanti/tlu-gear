84b6156eb133168ba1abdd37cdc70597acb54307
tlucanti <prototype3628800@yandex.ru>
2024-07-05 12:50:29 +0300

    libc: rewrite gcov/valgrind test
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

---
+++ libtest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ $1 == 'libmem' ]; then
+	NAME=LIBMEM
+	TARGET=./build/libmem_unittest.elf
+	DIR=libc/native/mem
+elif [ $1 == 'libstring' ]; then
+	NAME=LIBSTRING
+	TARGET=./build/libstring_unittest.elf
+	DIR=libc/native/string
+else
+	echo 'evaliable tests: libmem, libstring'
+	exit 1
+fi
+
+echo BUILDING $NAME FOR VALGRIND
+CONFIG_SILENT=1 make --environment-overrides --quiet clean
+CONFIG_SILENT=1 make --environment-overrides clean
+CONFIG_SANITIZE=0 make --environment-overrides --quiet -j $TARGET
+echo
+echo VALGRIND $NAME
+valgrind $TARGET
+
+
+echo
+echo BILDING $NAME FOR GCOV
+CONFIG_SILENT=1 make --environment-overrides --quiet clean
+CONFIG_SANITIZE=0 make --environment-overrides --quiet -j $TARGET
+echo
+$TARGET
+echo
+for f in $(ls ${DIR}/*.c); do
+	printf "GCOV $(basename $f):\t";
+	gcov $f | tail -n1 | grep -oP "\d+[.]\d+[%]";
+done
+
